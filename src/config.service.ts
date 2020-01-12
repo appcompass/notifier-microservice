733396b04f0201f62f905f42c409fb6d1f32d847
Jubair Saidi <jubair.saidi@gmail.com>
2020-01-12 11:06:58 -0500

    Creates code quality and tests github workflow

---
+++ src/config.service.ts
@@ -0,0 +1,43 @@
+import * as dotenv from 'dotenv';
+
+import * as Joi from '@hapi/joi';
+
+export type EnvConfig = Record<string, string>;
+
+export interface ValidConfig {
+  NODE_ENV: string;
+  REDIS_URL: string;
+  npm_package_name: string;
+  npm_package_gitHead: string;
+  npm_package_version: string;
+}
+
+export class ConfigService {
+  private readonly config: ValidConfig;
+  private schema: Joi.ObjectSchema = Joi.object({
+    NODE_ENV: Joi.string().default('local'),
+    REDIS_URL: Joi.string().default('redis://localhost:6379'),
+    npm_package_name: Joi.string(),
+    npm_package_gitHead: Joi.string(),
+    npm_package_version: Joi.string()
+  }).options({ stripUnknown: true });
+
+  constructor(path: string = `${process.env.NODE_ENV || 'local'}.env`) {
+    dotenv.config({ path });
+    this.config = this.validate(process.env);
+  }
+
+  private validate(config: EnvConfig): ValidConfig {
+    const { error, value } = this.schema.validate(config);
+
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+
+    return value;
+  }
+
+  public get<K extends keyof ValidConfig>(key: K): ValidConfig[K] {
+    return this.config[key];
+  }
+}
