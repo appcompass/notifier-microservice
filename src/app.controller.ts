c14668dc1a9a1ac00a75b645b8a3f3ec9e51bc27
Jubair Saidi <jubair.saidi@gmail.com>
2020-01-12 12:02:11 -0500

    Adds status message handler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,11 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @MessagePattern('notifier.status')
+  getStatus() {
+    return this.appService.getStatus();
+  }
+
   @MessagePattern('notifier.send.email')
   sendEmail(@Payload() payload: any) {
     return this.appService.send(payload);
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from './config.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   send(payload: any) {
     // TODO: Handle sending email.
     console.log(payload);
     return { payload };
+  }
+
+  getStatus() {
+    return {
+      serviceName: this.configService.get('npm_package_name'),
+      gitHash: this.configService.get('npm_package_gitHead'),
+      version: this.configService.get('npm_package_version')
+    };
   }
 }
