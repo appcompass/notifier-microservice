ab83518902250a7c6e6a75d9fc2667677e82e6f0
Jubair Saidi <jubair.saidi@gmail.com>
2020-01-12 11:48:19 -0500

    adds nest express package back in as it seems you can't create an app instance then add a microservice to it (this flow is needed to get the configService from the app when setting up the microservice) - unfortunate, but will update the code when/if they change that over @nestjs. fixes message pattern for sending emails subscriber.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,14 @@
 import { AppService } from './app.service';
 
 import { Controller } from '@nestjs/common';
-import {
-  MessagePattern,
-  Payload,
-  Ctx,
-  RedisContext
-} from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern('notifier.send.*')
-  sendEmail(@Payload() payload: any, @Ctx() context: RedisContext) {
-    console.log(context);
+  @MessagePattern('notifier.send.email')
+  sendEmail(@Payload() payload: any) {
     return this.appService.send(payload);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { ConfigService } from './config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.REDIS
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.REDIS,
+    options: {
+      url: configService.get('REDIS_URL')
+    }
   });
-  app.listen(() => new Logger('App').log('Microservice is listening'));
+
+  await app.startAllMicroservicesAsync();
 }
 bootstrap();
